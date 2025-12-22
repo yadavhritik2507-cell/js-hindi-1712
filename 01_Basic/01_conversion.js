e98ed947b083b11b0244671d7fdd68a7efa6e663
yadavhritik2507-cell <yadavhritik2507@gmail.com>
2025-12-22 15:11:22 +0000

    New File add

---
+++ 01_Basic/01_conversion.js
@@ -0,0 +1,23 @@
+let score = "33-23";
+ //console.log(typeof score);
+
+ let valueInNumber  = Number(score);
+//console.log(valueInNumber);
+ //console.log(typeof valueInNumber);
+
+let isLoggedIn = -1;
+//console.log(typeof isLoggedIn);
+let booleanisLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanisLoggedIn);
+ // "33" =33
+ // "33adc" = NaN
+ // true => 1 ; false => 0;
+// 1 => true ; 0 => false;
+// "" => false;
+// "JavaScript" => true
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123') 
+console.log(id==anotherId);
+
